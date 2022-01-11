e35962f523d7905b883f33c5797afeecd5fe9a78
BobbyHo <angela.bobby@gmail.com>
2022-01-11 21:53:02 +0800

    Add dynamic role iudgement & improve questions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Mustache from "mustache";
 import CryptoJS from "crypto-es";
 import { fetch } from "whatwg-fetch";
-import { getDetails } from "./questionsUtils";
+import { getDetails, getRole } from "./questionsUtils";
 import { registerEventListener } from "./eventListeners";
 
 const urlParams = new URLSearchParams(window.location.search);
@@ -15,6 +15,8 @@ const iv = "3de6a3000e65827d";
 
 let svcontent;
 let questions = [];
+let score = [0, 0]; //[本土語言, 行動傾向]
+let role;
 
 fetch(`https://www.surveycake.com/webhook/v0/${SVID}/${HASH}`)
 	.then((res) => res.text())
@@ -34,11 +36,19 @@ fetch(`https://www.surveycake.com/webhook/v0/${SVID}/${HASH}`)
 		svcontent = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
 
 		svcontent["result"].forEach((quiz) => {
-			questions.push({
-				subject: quiz.subject,
-				answer: quiz.answer,
-				detail: getDetails(quiz.sn),
-			});
+			if (quiz.answer.length != 0) {
+				questions.push({
+					subject: quiz.subject,
+					answer: quiz.answer,
+					detail: getDetails(quiz.sn),
+				});
+
+				let resScore = quiz.answerLabel[0].split(",").map((ele) => {return parseInt(ele)});
+
+				for (let i = 0; i < score.length; i++) {
+					score[i] = score[i] + resScore[i]
+				};
+			}
 		});
 
 		renderTemplate();
@@ -48,6 +58,11 @@ let renderTemplate = () => {
 	let main = document.getElementById("main");
 	let rendered = Mustache.render(main.innerHTML, {
 		questions: questions,
+		role: getRole(score),
+		score: {
+			lang: score[0],
+			action: score[1]
+		}
 	});
 	main.innerHTML = rendered;
 	registerEventListener();
--- src/questionsUtils.js
+++ src/questionsUtils.js
@@ -11,6 +11,31 @@ let getDetails = (sn) => {
     return(details[sn])
 }
 
+let getRole = (score) => {
+
+    let role
+
+    if (score[0] >= 6) { //本土語言者
+        if (score[1] >= 3) { //行動者
+            role = questionsData.roles[1]
+        } else {
+            role = questionsData.roles[0]
+        }
+    } else {
+        if (score[1] >= 3) {
+            role = questionsData.roles[3]
+        } else {
+            role = questionsData.roles[2]
+        }
+    }
+        
+    return({
+        roleName: role.title,
+        roleDescription: role.description
+    })
+}
+
 export {
-    getDetails
+    getDetails,
+    getRole
 }
